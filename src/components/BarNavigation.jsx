751e15dcc4ee7f6e7f64524bcbaa67e590b0c7e4
LUCASGOODPROGRAMMER <lucashubner2019@gmail.com>
2025-08-05 13:55:13 -0400

    estilização refatorada

--- src/components/BarNavigation.jsx
+++ src/components/BarNavigation.jsx
@@ -1,41 +1,16 @@
 import { NavLink } from "react-router-dom"
-// BarNavigation.jsx
 import "./BarNavigation.css"
 import SearchForm from "./SearchForm"
 
 const BarNavigation = () => {
-
-
     return (
-        <>
-            <div id="bar-navigation">
-                <SearchForm />
-                <NavLink
-                    to="cards"
-                >
-                    Cards
-                </NavLink>
-                <NavLink
-                    to="atividades"
-
-                >
-                    Atividades
-                </NavLink>
-                <NavLink
-                    to="material"
-                >
-                    Material
-                </NavLink>
-                <NavLink
-                    to="quiz"
-                >
-                    Quiz
-                </NavLink>
-
-            </div>
-
-
-        </>
+        <div id="bar-navigation">
+            <SearchForm />
+            <NavLink to="cards">Cards</NavLink>
+            <NavLink to="atividades">Atividades</NavLink>
+            <NavLink to="material">Material</NavLink>
+            <NavLink to="quiz">Quiz</NavLink>
+        </div>
     )
 }
 
